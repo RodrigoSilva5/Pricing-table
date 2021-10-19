8f8bba39fd7dd584fc7f7f2a987792fa7db25de0
rodrigo silva <89227598+RodrigoSilva5@users.noreply.github.com>
2021-10-19 18:40:48 -0300

    Update ambiente.js

--- ambiente.js
+++ ambiente.js
@@ -5,9 +5,9 @@ function changetables() {
     const price3 = document.querySelector("h2#price3")
 
     if(price1.innerHTML == '$12/month' ){
-        price1.innerHTML = '$144/month'
-        price2.innerHTML = '$192/month'
-        price3.innerHTML = '$220/month'
+        price1.innerHTML = '$144/Yearly'
+        price2.innerHTML = '$192/Yearly'
+        price3.innerHTML = '$220/Yearly'
     }else {
          price1.innerHTML = '$12/month'
         price2.innerHTML = '$16/month'
